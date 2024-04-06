23116c6aaa9f9475418ac2394be5e403e1d0621e
phuongvtb <bichphuong13042002@gmail.com>
2024-04-07 03:48:15 +0700

    Create Top List Tour in Home and fix some UI home

---
+++ src/pages/Home/TopListTour/style/StyleList.jsx
@@ -0,0 +1,16 @@
+// src/components/StyledList.js
+import React from 'react';
+import StyledCard from './StyleCard';
+
+const StyledList = ({ items }) => {
+    console.log('items',items);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {items.map((item) => (
+        <StyledCard key={item.id} data={item} />
+      ))}
+    </div>
+  );
+};
+
+export default StyledList;
