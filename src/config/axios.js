4e00108bf43f870dafb658c5982393d0d705d861
QuanDH <hongquan.contact@gmail.com>
2024-06-13 07:51:40 +0700

    commit fix url and json plan form

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 // const baseUrl = "http://localhost:8080";
-const baseUrl = 'https://travel-be.azurewebsites.net';
+const baseUrl = "http://localhost:5001";
 //
 const config = {
-    baseUrl,
-    timeout: 3000000,
+  baseUrl,
+  timeout: 3000000,
 };
 const api = axios.create(config);
 api.defaults.baseURL = baseUrl;
 const handleBefore = (config) => {
-    const token = localStorage.getItem('token')?.replaceAll('"', '');
-    config.headers['Authorization'] = `Bearer ${token}`;
-    return config;
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
+  config.headers["Authorization"] = `Bearer ${token}`;
+  return config;
 };
 const handleError = (error) => {
-    console.log(error);
-    return;
+  console.log(error);
+  return;
 };
 api.interceptors.request.use(handleBefore, handleError);
 // api.interceptors.response.use(null, handleError);
